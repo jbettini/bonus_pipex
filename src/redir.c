65c62710c0b55af2cf17b17ce5def64ce0d38bb4
Joseph Bettini <jbettini@c3r2p4.42nice.fr>
2022-02-18 19:58:01 +0100

    Last

--- src/redir.c
+++ src/redir.c
@@ -5,26 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jbettini <jbettini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/17 10:33:19 by jbettini          #+#    #+#             */
-/*   Updated: 2022/02/18 19:37:27 by jbettini         ###   ########.fr       */
+/*   Created: 2022/02/18 19:46:58 by jbettini          #+#    #+#             */
+/*   Updated: 2022/02/18 19:57:08 by jbettini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int redir_to_stdout(char *filename, int mod)
+int	redir_to_stdout(char *filename, int mod)
 {
-    int fd;
+	int	fd;
 
-    if (!mod)
-        fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0766);
-    else if (mod)
-        fd = open(filename, O_CREAT | O_RDWR | O_APPEND, 0766);
-    if (fd == -1)
-        return (-1);
-    if (dup2(fd, 1) == -1)
-        return (-1);
-    return (fd);
+	if (!mod)
+		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0766);
+	else if (mod)
+		fd = open(filename, O_CREAT | O_RDWR | O_APPEND, 0766);
+	if (fd == -1)
+		return (-1);
+	if (dup2(fd, 1) == -1)
+		return (-1);
+	return (fd);
 }
 
 void	pipex(char **args, t_env *env)
@@ -54,71 +54,71 @@ void	pipex(char **args, t_env *env)
 	}
 }
 
-int fd_stdin(char *filename, t_env *env)
+int	fd_stdin(char *filename, t_env *env)
 {
-    int fd;
+	int	fd;
 
-    fd = -1;
-    if (access(filename, F_OK | R_OK) == -1)
-    {
-        perror("file not found");
-        return (fd);
-    }
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        exec_error(OP_ERROR);
-    if (dup2(fd, 0) == -1)
-        exec_error(DUP_ERROR);
-    env->redir = 0;
-    return (0); 
+	fd = -1;
+	if (access(filename, F_OK | R_OK) == -1)
+	{
+		perror("file not found");
+		return (fd);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		exec_error(OP_ERROR);
+	if (dup2(fd, 0) == -1)
+		exec_error(DUP_ERROR);
+	env->redir = 0;
+	return (0);
 }
 
-int fd_heredoc(char *stop, t_env *env)
+int	fd_heredoc(char *stop, t_env *env)
 {
-    int     fd;
-    char    **hd;
-    int     i;  
+	int		fd;
+	char	**hd;
+	int		i;	
 
-    i = -1;
-    fd = open(".heredoc_tmp", O_CREAT | O_RDWR | O_TRUNC, 0744);
-    hd = heredoc(stop);
-    while (hd[++i])
-        ft_putstr_fd(hd[i], fd);
-    close(fd);
-    ft_free_split(hd);
-    fd = open(".heredoc_tmp", O_RDONLY);
-    if (fd == -1)
-        exec_error(OP_ERROR);
-    if (dup2(fd, 0) == -1)
-        exec_error(DUP_ERROR);
-    env->redir = 1;
-    return (1);
+	i = -1;
+	fd = open(".heredoc_tmp", O_CREAT | O_RDWR | O_TRUNC, 0744);
+	hd = heredoc(stop);
+	while (hd[++i])
+		ft_putstr_fd(hd[i], fd);
+	close(fd);
+	ft_free_split(hd);
+	fd = open(".heredoc_tmp", O_RDONLY);
+	if (fd == -1)
+		exec_error(OP_ERROR);
+	if (dup2(fd, 0) == -1)
+		exec_error(DUP_ERROR);
+	env->redir = 1;
+	return (1);
 }
 
-char **heredoc(char *stop)
+char	**heredoc(char *stop)
 {
-    t_list  *hd;
-    char    **ret;
-    char    *line;
-    int	    check;
+	t_list	*hd;
+	char	**ret;
+	char	*line;
+	int		check;
 
-    hd = NULL;
-    ret = NULL;
-    line = NULL;
-    check = 1;
-    stop = ft_strjoin(stop, "\n");
-    while (check == 1)
-    {
-        ft_putstr("> ");
-        line = get_next_line(0);
-        if (!ft_strequ(line, stop))
+	hd = NULL;
+	ret = NULL;
+	line = NULL;
+	check = 1;
+	stop = ft_strjoin(stop, "\n");
+	while (check == 1)
+	{
+		ft_putstr("> ");
+		line = get_next_line(0);
+		if (!ft_strequ(line, stop))
 			ft_lstadd_back(&hd, ft_lstnew(ft_strdup(line)));
 		else
-			check--; 
-        free (line);
-    }
-    ret = ft_lst_to_dpt(hd);
-    ft_lstclear(&hd, free);
-    free(stop);
-    return (ret);
+			check--;
+		free (line);
+	}
+	ret = ft_lst_to_dpt(hd);
+	ft_lstclear(&hd, free);
+	free(stop);
+	return (ret);
 }
